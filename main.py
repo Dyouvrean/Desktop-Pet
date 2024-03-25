2bcdd7b7653e7a1d53329811cc2011b10d0e7fc7
Dyouvrean <dry199984@outlook.com>
2024-03-24 23:52:42 -0400

    run back and forth

--- main.py
+++ main.py
@@ -13,10 +13,12 @@ class DesktopPet(QWidget):
         super(DesktopPet, self).__init__(parent)
         self.action_images = None
         self.up_down = False
-        self.is_running_action = False
+        self.is_running = False
         self.is_follow_mouse = False
         self.index = 0
         self.action_distribution=[...]
+        self.directionX = 8 # Adjust for speed and direction
+        self.directionY = 5  # Adjust for speed and direction
         self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint | Qt.SubWindow)
         self.setAutoFillBackground(False)
         self.setAttribute(Qt.WA_TranslucentBackground, True)
@@ -27,11 +29,30 @@ class DesktopPet(QWidget):
         self.pet_images, iconpath = self.loadPetImages()
         self.image = QLabel(self)
         self.image.setPixmap(self.pet_images[0][0])
+        self.running_l_images = self.loadRunning_l_Images()
+        self.running_r_images = self.loadRunning_r_Images()
+        self.current_frame =0
         self.move_timer = QTimer()
+
         # self.commonAction()
         self.show()
 
-
+    def rightMenu(self,pos):
+        self.move_timer.stop()
+        self.myMenu = QMenu(self)
+        self.actionA = QAction(QIcon("来回跑"), "来回跑", self)
+        self.actionA.triggered.connect(self.moveleftRight)
+        self.actionB = QAction(QIcon("停止"), "停止", self)
+        self.actionB.triggered.connect(self.moveStop)
+        self.actionC = QAction(QIcon("睡觉"), "睡觉", self)
+        self.actionC.triggered.connect(self.moveSleep)
+        self.actionD = QAction(QIcon("退出"), "退出", self)
+        self.actionD.triggered.connect(self.quit)
+        self.myMenu.addAction(self.actionA)
+        self.myMenu.addAction(self.actionB)
+        self.myMenu.addAction(self.actionC)
+        self.myMenu.addAction(self.actionD)
+        self.myMenu.popup(QCursor.pos())
     def loadImage(self, imagepath):
         image = QPixmap()
         image.load(imagepath)
@@ -46,6 +67,22 @@ class DesktopPet(QWidget):
         iconpath = os.path.join("4", 'shime1.png')
         return pet_images, iconpath
 
+    def loadRunning_l_Images(self):
+        runing=[]
+        for i in range(1, 3):  # Assuming N frames in the animation
+            runing.append(self.pet_images[i][0])
+        return runing
+    def loadRunning_r_Images(self):
+        runing=[]
+        for i in range(33, 35):  # Assuming N frames in the animation
+            runing.append(self.pet_images[i][0])
+        return runing
+
+    def commonAction(self):
+        # 每隔一段时间做个动作
+        self.timer_common = QTimer()
+        self.timer_common.timeout.connect(self.randomAct)
+        self.timer_common.start(1000)
     def randomAct(self):
         self.pet_images, iconpath = self.loadPetImages()
         if not self.is_running_action:
@@ -63,11 +100,7 @@ class DesktopPet(QWidget):
         self.image.setPixmap(self.action_images[self.action_pointer])
         self.action_pointer += 1
 
-    def commonAction(self):
-        # 每隔一段时间做个动作
-        self.timer_common = QTimer()
-        self.timer_common.timeout.connect(self.randomAct)
-        self.timer_common.start(1000)
+
 
     def selfMoveAction(self):
         try:
@@ -86,21 +119,7 @@ class DesktopPet(QWidget):
         except Exception as e:
             print(e)
 
-    def rightMenu(self,pos):
-        self.myMenu = QMenu(self)
-        self.actionA = QAction(QIcon("移动"), "移动", self)
-        self.actionA.triggered.connect(self.moveUpDown)
-        self.actionB = QAction(QIcon("停止"), "停止", self)
-        self.actionB.triggered.connect(self.moveStop)
-        self.actionC = QAction(QIcon("睡觉"), "睡觉", self)
-        self.actionC.triggered.connect(self.moveSleep)
-        self.actionD = QAction(QIcon("退出"), "退出", self)
-        self.actionD.triggered.connect(self.quit)
-        self.myMenu.addAction(self.actionA)
-        self.myMenu.addAction(self.actionB)
-        self.myMenu.addAction(self.actionC)
-        self.myMenu.addAction(self.actionD)
-        self.myMenu.popup(QCursor.pos())
+
 
     def mousePressEvent(self, event):
         if event.button() == Qt.LeftButton:
@@ -124,21 +143,54 @@ class DesktopPet(QWidget):
         self.setCursor(QCursor(Qt.ArrowCursor))
 
 
-    def moveUpDown(self):
+    def moveleftRight(self):
+        self.move_timer.timeout.connect(self.updateLeft_Right_Position)
         self.move_timer.start(100)
-        self.up_down = True
-        self.timer_common.start(500)
-        self.timer_sleep.stop()
 
+    def updateAnimationFrame(self):
+        if not self.is_running:
+            return
 
-    def upAndDown(self):
-        print("up ")
-        if self.up_down:
-            self.stop_threads = False
-            t = threading.Thread(target=self.do, args={})
-            t.start()
+        if self.directionX>0:
+            self.current_frame = (self.current_frame + 1) % len(self.running_r_images)
+            self.image.setPixmap(self.running_r_images[self.current_frame])
         else:
-            self.stop_threads = True
+            self.current_frame = (self.current_frame + 1) % len(self.running_l_images)
+            self.image.setPixmap(self.running_l_images[self.current_frame])
+    def updateLeft_Right_Position(self):
+        self.is_running= True
+        screenWidth = QApplication.desktop().width()
+
+        newX = self.x() + self.directionX
+        newY = self.y()
+        # Reverse direction if the pet hits the screen edge
+        if newX < 0 or newX + self.width() > screenWidth:
+            self.directionX = -self.directionX
+            newX += 2 * self.directionX
+        self.move(newX, newY)
+        self.updateAnimationFrame()
+
+    def updatePosition(self):
+        self.is_running= True
+        screenWidth = QApplication.desktop().width()
+        screenHeight = QApplication.desktop().height()
+
+        newX = self.x() + self.directionX
+        newY = self.y() + self.directionY
+
+        # Reverse direction if the pet hits the screen edge
+        if newX < 0 or newX + self.width() > screenWidth:
+            self.directionX = -self.directionX
+            newX += 2 * self.directionX
+
+        if newY < 0 or newY + self.height() > screenHeight:
+            self.directionY = -self.directionY
+            newY += 2 * self.directionY
+
+        self.move(newX, newY)
+        self.updateAnimationFrame()
+
+
 
     def moveStop(self):
         print("Stop")
